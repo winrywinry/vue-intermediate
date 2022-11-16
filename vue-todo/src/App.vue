04a7574520a714b183fffa7dacbc67a3076b1d88
suyeon <yunsuyeon@outlook.kr>
2022-11-16 13:10:27 +0900

    init Commit

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <todo-header></todo-header>
+    <todo-input></todo-input>
+    <todo-list></todo-list>
+    <todo-footer></todo-footer>
+  </div>
+</template>
+
+<script>
+import TodoFooterVue from './components/TodoFooter.vue'
+import TodoHeaderVue from './components/TodoHeader.vue'
+import TodoInputVue from './components/TodoInput.vue'
+import TodoListVue from './components/TodoList.vue'
+
+export default {
+  components: {
+    'todo-header': TodoHeaderVue,
+    'todo-input': TodoInputVue,
+    'todo-list': TodoListVue,
+    'todo-footer': TodoFooterVue
+  }
+}
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #f6f6f6;
+}
+
+input {
+  border-style: groove;
+  width: 200px;
+}
+
+button {
+  border-style: groove;
+}
+
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
